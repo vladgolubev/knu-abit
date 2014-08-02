893895f9f247b7bcbd205fc868a185dc08f14854
samosfator <golubev.vld@yandex.ru>
2014-08-02 06:43:36 +0300

    Added GA

--- src/ua/samosfator/knu/abit/HTML.java
+++ src/ua/samosfator/knu/abit/HTML.java
@@ -86,7 +86,12 @@ public class HTML {
     }
 
     private static String getHtmlAfterBody(int nestingLevel) {
-        return "<script src=\"" + createDots(nestingLevel) + "js/script.js\"></script></html>";
+        return "<script src=\"" + createDots(nestingLevel) + "js/script.js\"></script>" +
+                "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q" +
+                "||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.a" +
+                "sync=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.c" +
+                "om/analytics.js','ga'); ga('create', 'UA-46344654-6', 'auto'); ga('send', 'pageview');</script>" +
+                "</html>";
     }
 
     private static String createDots(int level) {
